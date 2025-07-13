b1d7c18884e39eccd1b2d4a227379d2b03293b8b
SoggyMousepad <soggymousepad@gmail.com>
2025-07-13 17:56:28 +0100

    ⛏️Fix typings

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js')
 
 const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_ANON_KEY
+const supabaseKey = process.env.SUPABASE_KEY
 
 if (!supabaseUrl || !supabaseKey) {
   console.error('Missing Supabase environment variables')
